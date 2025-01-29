cb16fa05e96c8be18c982abfc65b1b98ab18d471
HaAymar <he201905@students.ephec.be>
2025-01-29 23:27:25 +0100

    Delete the previous backend

--- Backend/ehealthCrud/src/main/java/com/example/ehealth/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ehealth.repository;
-
-import com.example.ehealth.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
