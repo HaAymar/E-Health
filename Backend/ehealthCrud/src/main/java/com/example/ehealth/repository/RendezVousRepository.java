799fcaefa6417f3c288cd205591c61da16d104a9
HaAymar <he201905@students.ephec.be>
2025-01-29 23:19:59 +0100

    update backend

--- Backend/ehealthCrud/src/main/java/com/example/ehealth/repository/RendezVousRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ehealth.repository;
-
-import com.example.ehealth.entity.RendezVous;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RendezVousRepository extends JpaRepository<RendezVous, Long> {
-}
