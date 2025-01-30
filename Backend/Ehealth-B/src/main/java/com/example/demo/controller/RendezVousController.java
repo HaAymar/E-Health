420a0289604b754eee70b73dee238c3c6ca0ee03
Teodora Miteva <teodora.mtv@gmail.com>
2025-01-30 11:37:56 +0100

    Modification RDVController, RDVEntity

--- Backend/Ehealth-B/src/main/java/com/example/demo/controller/RendezVousController.java
+++ Backend/Ehealth-B/src/main/java/com/example/demo/controller/RendezVousController.java
@@ -62,5 +62,14 @@ public class RendezVousController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    @PatchMapping("/{id}/description")
+        public ResponseEntity<?> updateDescription(@PathVariable Long id, @RequestParam String description) {
+            try {
+                RendezVous updatedRendezVous = rendezVousService.updateStatut(id, description);
+                return ResponseEntity.ok(updatedRendezVous);
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
     }
 }
